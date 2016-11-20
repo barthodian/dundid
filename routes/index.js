a2ff508ee71cb96b0090961a2b1247b8e5370d40
Barthodian <barthodian@gmail.com>
2016-11-19 19:10:02 -0500

    Built basic structure

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const fs = require('fs')
+
+router.get('/', (req, res, next)=>{
+  res.render('index', { 
+      title: 'Dundid' 
+  })
+})
+
+router.post('/', (req, res, next)=>{
+    fs.readFile("./testfile", "utf-8", (err, data)=>{
+        if (err) throw err
+        console.log(data)
+    })
+    console.log(req.body.dunthis, req.body.important)
+})
+
+module.exports = router
